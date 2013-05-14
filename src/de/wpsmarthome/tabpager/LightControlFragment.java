dc232898ccadb086b5805f54ecac682a224b3a26
Till Theis <theis.till@gmail.com>
2013-05-15 00:10:39 +0200

    improved kitchen light fragment+view

--- src/de/wpsmarthome/tabpager/LightControlFragment.java
+++ src/de/wpsmarthome/tabpager/LightControlFragment.java
@@ -1,15 +1,24 @@
 package de.wpsmarthome.tabpager;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
+import android.view.View.OnLongClickListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
-import android.widget.TextView;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class LightControlFragment extends ControlFragment {
+    
+    private final String simpleClassName = getClass().getSimpleName();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,17 +30,30 @@ public class LightControlFragment extends ControlFragment {
             Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_control_light,
          container, false);
-        if (mContext != null && mControl != null) {
-            TextView tw = (TextView) rootView.findViewById(R.id.title);
-            tw.setText(mContext + " " + mControl);
-        }
         
-        CompoundButton toggleButton = (CompoundButton) rootView.findViewById(R.id.toggleButton);
-        toggleButton.setOnClickListener(new OnClickListener() {
-            
+        CompoundButton toggleButton = (CompoundButton) rootView.findViewById(R.id.lightToggleButton);
+        toggleButton.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.d(simpleClassName, String.format("toggleButton.onCheckedChanged(this, %b)", isChecked));
+            }
+        });
+        
+        View dimmerLabelGroup = (View) rootView.findViewById(R.id.lightDimmerLabelGroup);
+        dimmerLabelGroup.setOnClickListener(new OnClickListener() {
+            @SuppressWarnings("deprecation")
             @Override
             public void onClick(View v) {
-                Log.d("TAG", "onclick");
+                Log.d(simpleClassName, "dimmerLabelGroup.onClick(v)");
+            }
+        });
+        
+        View colorLabelGroup = (View) rootView.findViewById(R.id.lightColorLabelGroup);
+        colorLabelGroup.setOnClickListener(new OnClickListener() {
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onClick(View v) {
+                Log.d(simpleClassName, "colorLabelGroup.onClick(v)");
             }
         });
 
